559c6ed4b74c20c22b8590d446cf0cdc9a4b39cd
Kata Marton <kata.marton@stud.uni-corvinus.hu>
2023-11-28 08:35:58 +0100

    save

---
+++ WebAPIBase/wwwroot/scripts/geek.js
@@ -0,0 +1,19 @@
+﻿window.onload = function () {
+    letöltés();
+    letöltésBefejeződött(k)
+}
+
+var viccek;
+
+function letöltés() {
+fetch('/jokes.json')
+    .then(response => response.json())
+    .then(data => letöltésBefejeződött(data)
+    );
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+}
