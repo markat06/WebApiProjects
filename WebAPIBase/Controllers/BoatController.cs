863f584e3f61149dd5b56e2d5ddd1cf99201c60d
Kata Marton <kata.marton@stud.uni-corvinus.hu>
2023-12-05 09:14:51 +0100

    s

--- WebAPIBase/Controllers/BoatController.cs
+++ WebAPIBase/Controllers/BoatController.cs
@@ -8,14 +8,28 @@ namespace WebAPIBase.Controllers
 
     public class BoatController : Controller
     {
+        //[HttpGet]
+        //[Route("questions/all")]
+        //public IActionResult MindegyHogyHivjak()
+        //{
+        //    HajosContext context = new HajosContext();
+        //    var kérdések = from x in context.Questions select x.Question1;
+
+        //    return Ok(kérdések);
+        //}
+
         [HttpGet]
-        [Route("questions/all")]
-        public IActionResult MindegyHogyHivjak()
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
         {
             HajosContext context = new HajosContext();
-            var kérdések = from x in context.Questions select x.Question1;
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
 
-            return Ok(kérdések);
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
         }
     }
 }
---
+++ WebAPIBase/wwwroot/scripts/main.js
@@ -0,0 +1,18 @@
+﻿fetch('/questions/4')
+    .then(response => response.json())
+    .then(data => console.log(data)
+);
+
+fetch('/questions/1')
+    .then(response => response.json())
+    .then(data => kérdésMegjelenítés(data)
+    );
+
+function kérdésMegjelenítés(kérdés) {
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+}
