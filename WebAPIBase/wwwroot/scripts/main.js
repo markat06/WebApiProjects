8d3ece2ee7b30ede27b88060f2cd01553752e148
Kata Marton <kata.marton@stud.uni-corvinus.hu>
2023-12-05 09:25:45 +0100

    sa

--- WebAPIBase/wwwroot/scripts/main.js
+++ WebAPIBase/wwwroot/scripts/main.js
@@ -3,6 +3,9 @@
     .then(data => console.log(data)
 );
 
+var jóVálasz;
+var questionId = 4
+
 fetch('/questions/1')
     .then(response => response.json())
     .then(data => kérdésMegjelenítés(data)
@@ -15,4 +18,63 @@ function kérdésMegjelenítés(kérdés) {
     document.getElementById("válasz2").innerText = kérdés.answer2
     document.getElementById("válasz3").innerText = kérdés.answer3
     document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+}
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                kérdésMegjelenítés(response.json())
+            }
+        })
+} 
+
+function kérdésMegjelenítés(kérdés) {
+    if (!kérdés) return; //Ha undefined a kérdés objektum, nincs mit tenni
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    jóVálasz = kérdés.correctAnswer;
+    if (kérdés.image) {
+        document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+        document.getElementById("kép").classList.remove("rejtett")
+    }
+    else {
+        document.getElementById("kép").classList.add("rejtett")
+    }
+    //Jó és rossz kérdések jelölésének levétele
+    document.getElementById("válasz1").classList.remove("jó", "rossz");
+    document.getElementById("válasz2").classList.remove("jó", "rossz");
+    document.getElementById("válasz3").classList.remove("jó", "rossz");
+}
+
+function előre() {
+    questionId++;
+    kérdésBetöltés(questionId)
+}
+
+function vissza() {
+    questionId--;
+    kérdésBetöltés(questionId)
+}
+window.onload = function (e) {
+    console.log("Oldal betöltve...");
+    document.getElementById("előre_gomb").onclick = előre;
+    document.getElementById("vissza_gomb").onclick = vissza;
+    kérdésBetöltés(questionId)
+}
+
+function választás(n) {
+    if (n != jóVálasz) {
+        document.getElementById(`válasz${n}`).classList.add("rossz");
+        document.getElementById(`válasz${jóVálasz}`).classList.add("jó");
+    }
+    else {
+        document.getElementById(`válasz${jóVálasz}`).classList.add("jó");
+    }
 }
